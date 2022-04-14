19dff9fb6ff09dffefc35797316e9c887852f460
Sabiha Bary <sabihaab9@gmail.com>
2022-04-14 01:26:45 -0400

    commit tree 960a8d44d26af3fb837670ab9ee9adb253caedae

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    
+    string = string.toUpperCase();
+
+    console.log(string);
+}
+function logWhisper(string){
+    
+    string = string.toLowerCase();
+
+    console.log(string);
+}
+
+function sayHiToHeadphonedRoommate(string){
+
+    if(string === string.toLowerCase()){
+
+        return ("I can't hear you!");
+
+    }
+    else if(string === string.toUpperCase()){
+
+        return( "YES INDEED!")
+    }
+
+    else if(string === "Let's have dinner together!"){
+
+        return( "I would love to!")
+    }
+
+}
